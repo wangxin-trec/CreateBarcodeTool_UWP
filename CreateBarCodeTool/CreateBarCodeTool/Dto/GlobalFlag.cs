e61ecd0191ca8309678115e8ecdd5073790f385f
wangxin-trec <10153702wang_xin@cn.tre-inc.com>
2020-09-17 20:48:46 +0800

    显示帮助说明改为从文件读取

---
+++ CreateBarCodeTool/CreateBarCodeTool/Dto/GlobalFlag.cs
@@ -0,0 +1,25 @@
+﻿namespace CreateBarCodeTool.Dto {
+    class GlobalFlag {
+
+        private string help = "";
+
+        private static GlobalFlag singoton;
+
+        private GlobalFlag() { }
+
+        public static GlobalFlag getInstance() {
+            if (singoton == null) {
+                singoton = new GlobalFlag();
+            }
+            return singoton;
+        }
+
+        public void setHelp(string help) {
+            this.help = help;
+        }
+
+        public string getHelp() {
+            return this.help;
+        }
+    }
+}
